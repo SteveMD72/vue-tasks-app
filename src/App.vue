1c47116a4842ee9be221f6dcb7bf7f8a6a509b57
Steve Denning <steve.denning@cgi.com>
2025-06-18 10:37:15 +0100

    reset to base files

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,9 @@
-<script setup lang="ts">
-import { ref } from "vue";
-
-// Components
-import TaskForm from "./components/TaskForm.vue";
-
-// Types
-import type { Task } from "./types";
-
-// State
-const message = ref("Tasks App");
-const tasks = ref<Task[]>([]);
-
-// Methods
-const addTask = (newTask: string) => {
-  tasks.value.push({
-    id: crypto.randomUUID(),
-    title: newTask,
-    done: false,
-  });
-};
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <h1>{{ message }}</h1>
-    <TaskForm @add-task="addTask" />
-  </main>
-  <h2>Tasks</h2>
-  <ul>
-    <li v-for="task in tasks" :key="task.id">
-      {{ task.title }} - {{ task.done ? "Done" : "Pending" }}
-    </li>
-  </ul>
+  <header>Header</header>
+  <main>Body</main>
+  <footer>Footer</footer>
 </template>
 
-<style scoped>
-main {
-  max-width: 1200px;
-  margin: 1rem auto;
-}
-</style>
+<style scoped></style>
